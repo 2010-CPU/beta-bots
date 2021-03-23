b22f840dbc4fc3176992b24d3e128bc2034cd11c
Meagan Fugate <meaganfugate@gmail.com>
2021-03-23 16:35:27 -0700

    Edit#1000 for issue#4

--- db/init_db.js
+++ db/init_db.js
@@ -34,7 +34,7 @@ async function buildTables() {
           id SERIAL PRIMARY KEY,
           "firstName" VARCHAR(255) NOT NULL,
           "lastName" VARCHAR(255) NOT NULL,
-          email VARCHAR(255)UNIQUE NOT NULL,
+          email VARCHAR(255) UNIQUE NOT NULL,
           "imageURL" TEXT DEFAULT "placeimg.com/1",
           username VARCHAR(255) UNIQUE NOT NULL,
           password VARCHAR(255) UNIQUE NOT NULL,
