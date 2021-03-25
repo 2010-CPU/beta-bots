560632ba9df325f4197ae8374be4c26235ba4f42
Meagan Fugate <meaganfugate@gmail.com>
2021-03-24 20:08:52 -0700

    CreateProduct db ready for review for issue#16

--- db/index.js
+++ db/index.js
@@ -5,9 +5,29 @@ const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
 const client = new Client(DB_URL);
 
 // database methods
-
+const createProduct = async (productField) => {
+  const { name, description, price, category } = productField;
+  if (!name || !description || !price || !category) {
+    throw new Error ("Product is missing information!")
+  }
+  const insertString = Object.keys(productField).join(", ")
+  const sqlInsert = Object.keys(productField).map((key, index) => {
+    return `$${index + 1}`
+  }).join(", ")
+  try {
+      const { rows: [product] } = await client.query(`
+          INSERT INTO products(${insertString})
+          VALUES(${sqlInsert})
+          RETURNING *;
+      `, Object.values(productField));
+      return product;
+  } catch (error) {
+      throw error
+  };
+};
 // export
 module.exports = {
   client,
+  createProduct,
   // db methods
 }
