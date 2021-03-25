ce4236d9e004d528bd84f99d87fa7b8edab76025
irationsal <irationsal@gmail.com>
2021-03-24 19:58:03 -0700

    Finished making getProductById. Closes #14

--- db/index.js
+++ db/index.js
@@ -5,9 +5,24 @@ const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
 const client = new Client(DB_URL);
 
 // database methods
+const getProductById = async (id) => {
+  try {
+    const {rows: [product]} = await client.query(`
+      SELECT *
+      FROM products
+      WHERE id = $1;
+    `, [id])
 
+    console.log(product)
+
+    return product
+  } catch (error) {
+    console.log(error)  
+  }
+}
 // export
 module.exports = {
   client,
   // db methods
+  getProductById,
 }
