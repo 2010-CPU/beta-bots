ae20143ef61d8a28e0bf2b464a493b03119c79dd
Meagan Fugate <meaganfugate@gmail.com>
2021-03-25 18:55:42 -0700

    Commit merge

---
+++ src/components/Product.js
@@ -0,0 +1,10 @@
+
+const API_URL = `https://localhost:5000`
+
+const Product = () => {
+ const [products, setProducts] = useState({});
+    const fetchAllProducts = async () => {
+        const response = await fetch(``)
+    }
+
+}
