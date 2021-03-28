e6deccfe8d16d64529050bee63b6ca6510a0a70d
Meagan Fugate <meaganfugate@gmail.com>
2021-03-27 18:41:05 -0700

    Users DB Adapters completed for review and resolve of issue#27

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ const { Client } = require('pg');
 const DB_NAME = 'graceshopper'
 const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
 const client = new Client(DB_URL);
+const bcrypt = require('bcrypt');
 
 // database methods
 const createProduct = async (productField) => {
@@ -58,9 +59,8 @@ const createUser = async ({firstName, lastName, email, username, password}) => {
     const SALT_COUNT = 10;
     const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
     const { rows: [user] } = await client.query(`
-    INSERT INTO users("firstName", "lastName", enail, username, password)
+    INSERT INTO users("firstName", "lastName", email, username, password)
     VALUES($1, $2, $3, $4, $5)
-    ON CONFLICT ("firstName", username) DO NOTHING
     RETURNING *;
     `, [firstName, lastName, email, username, hashedPassword]);
     delete user.password
@@ -70,7 +70,73 @@ const createUser = async ({firstName, lastName, email, username, password}) => {
   }
 }
 
-  
+const getUser = async ({username, password}) => {
+  try {
+    const user = await getUserByUsername(username);
+ if (!user){
+   throw new Error ("No user by that username!")
+ } else {
+   const hashedPassword = user.password;
+   const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+   
+   if(passwordsMatch){
+     delete user.password
+     return user
+   }
+ }
+  } catch (error) {
+    throw error
+  }
+
+}
+const getUserByUsername = async(username) => {
+  try {
+    const { rows: [user] } = await client.query(`
+    SELECT *
+    FROM users
+    WHERE username=$1;
+    `, [username])
+
+    return user
+  } catch (error) {
+    throw error
+  }
+};
+
+const getAllUsers = async() => {
+  try {
+    const { rows: users } = await client.query(`
+      SELECT 
+        users.id,
+        users."firstName",
+        users."lastName",
+        users.email,
+        users."imageURL",
+        users.username,
+        
+        users.password,
+        users."isAdmin"
+      FROM users;
+    `)
+    return users
+  } catch (error) {
+    throw error
+  }
+}
+
+const getUserById = async(id) => {
+  try {
+    const { rows: [user] } = await client.query(`
+    SELECT *
+    FROM users
+    WHERE id=$1;
+    `, [id]);
+    delete user.password
+    return user
+  } catch (error) {
+    throw error
+  }
+}
   // export
 module.exports = {
   client,
@@ -78,4 +144,8 @@ module.exports = {
   getProductById,
   getAllProducts,
   createUser,
+  getUserByUsername,
+  getUser,
+  getAllUsers,
+  getUserById
 }
