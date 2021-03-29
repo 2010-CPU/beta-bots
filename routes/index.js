1946370a0570da061952b2cf2d8db5f62be763e0
Alexandra Hargrove <alexandrahargrove@live.com>
2021-03-28 17:43:31 -0700

    Set up users routes. Register, login, and me. Tested with Postman

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,41 @@
+require('dotenv').config();
 const apiRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = process.env;
+const {
+  getUserById
+} = require('../db');
+
+
+
+apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length)
+    try {
+      const {id} = jwt.verify(token, JWT_SECRET);
+      if (id) {
+        req.user = await getUserById(id)
+        next();
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+})
+
+
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log('user is set', req.user);
+  }
+  next();
+})
+
+
 
 apiRouter.get("/", (req, res, next) => {
   res.send({
@@ -8,5 +45,16 @@ apiRouter.get("/", (req, res, next) => {
 
 const productsRouter = require('./products')
 apiRouter.use('/products', productsRouter)
+
+const usersRouter = require('./users')
+apiRouter.use('/users', usersRouter);
+
+apiRouter.get("*", (req, res, next) => {
+  res.status(404).send({message: '404 not found'});
+})
+
+apiRouter.use((error, req, res, next) => {
+  res.status(500).send({error});
+})
 
 module.exports = apiRouter;
