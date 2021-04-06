293439e90a1be48c18da6498e3be551aea398afa
Meagan Fugate <meaganfugate@gmail.com>
2021-04-06 16:37:06 -0700

    Separating product components

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,9 +3,10 @@ import { useParams } from 'react-router-dom'
 import {fetchProductById} from '../api';
 
 const Product = (props) => {
-    const {token, product} = props
+
+    const {token} = props
     const { productId } = useParams()
-    const [_product, setProduct] = useState({})
+    const [product, setProduct] = useState({})
     const fetchSingleProduct = async () => {
         try{
             const product = await fetchProductById(productId)
@@ -24,20 +25,22 @@ const Product = (props) => {
     }, [token]);
     
  return (
-     product ? <div className='product'>
-        <a href ={`products/${product.id}`}>
+    //  product ? 
+    //  <div className='product'>
+    //     <a href ={`products/${product.id}`}>
+    //     <img src={`${product.imageURL} ? ${product.id}`} alt={product.name}/>      
+    //     <p>{product.name}</p>
+    //     </a>
+    //     <p>${product.price}</p>
+    //     <p>{product.category}</p>
+    // </div> 
+    //  : 
+    <div className='product'>
         <img src={`${product.imageURL} ? ${product.id}`} alt={product.name}/>      
         <p>{product.name}</p>
-        </a>
+        <p>{product.description}</p>
         <p>${product.price}</p>
         <p>{product.category}</p>
-    </div>  : 
-    <div className='product'>
-        <img src={`${_product.imageURL} ? ${_product.id}`} alt={_product.name}/>      
-        <p>{_product.name}</p>
-        <p>{_product.description}</p>
-        <p>${_product.price}</p>
-        <p>{_product.category}</p>
     </div>
     )
 }
