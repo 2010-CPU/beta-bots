263858484ef2747f88c498fc0ab89f0558ae59c6
Evan-Hoyle <ehoyleconsulting@gmail.com>
2021-04-10 14:20:44 -0500

    products POST complete

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const productsRouter = require('express').Router()
-const {getAllProducts, getProductById} = require('../db')
+const {getAllProducts, getProductById, createProduct} = require('../db')
+const {requireAdmin} = require('./utils')
 
 productsRouter.get('/', async (req, res, next) => {
     try {
@@ -19,6 +20,25 @@ productsRouter.get('/:productId', async (req, res, next) => {
         } else {
             next({error: "Product doesn't exist!"})
         }
+    } catch (error) {
+        next({error})
+    }
+})
+
+productsRouter.post('/', requireAdmin, async (req, res, next) => {
+    try {
+        const {name, description, price, imageURL, category, inStock} = req.body;
+        const productToCreate = {name, description, price, category}
+        if (imageURL) {
+            productToCreate.imageURL = imageURL;
+        }
+        if (inStock) {
+            productToCreate.inStock = inStock;
+        }
+        const product = await createProduct(productToCreate);
+        if (product) {
+            res.send({product})
+        } 
     } catch (error) {
         next({error})
     }
